721a91250f6f41143e93b557203c0ded6f3f37e1
leonbantin <lp_leon@hotmail.de>
2017-11-06 22:48:49 +0100

    Rename createLobby.js

--- js/createLobby.js
+++
@@ -1,6 +0,0 @@
-let slider = document.querySelector(".js-game-length-slider");
-let valueDiv = document.querySelector(".js-game-length");
-
-slider.addEventListener('input', () => {
-   valueDiv.innerHTML = slider.value; 
-});
