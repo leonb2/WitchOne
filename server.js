ac87841a681b52dcdf94fa1521e47392be0d2dbe
leonbantin <lp_leon@hotmail.de>
2017-11-09 11:32:27 +0100

    Seperate socketIO logic and implement ready counter

--- server.js
+++ server.js
@@ -16,8 +16,6 @@ app.use(session({
     saveUninitialized: true
 }));
 
-const socketio = require('socket.io');
-
 const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({extended:true}));
 
@@ -39,7 +37,8 @@ const server = app.listen(port, () => {
    console.log(`Server started and is listening to ${port}`);
 });
 
-const io = socketio(server);
+const socketScript = require(__dirname + '/socketIO.js');
+socketScript.initialize(server);
 const rooms = [];
 
 /// --- Website Logic ---
@@ -177,9 +176,11 @@ app.post('/createLobbyPost', (request, response) => {
         'password' : lobbyPassword,
         'gameLength' : gameLengthSec,
         'users' : [],
-        'userIDs' : []
+        'userIDs' : [],
+        'usersReady': 0
     };
     rooms.push(room);
+    socketScript.addRoom(room);
     console.log("Lobby was created.")
     
     request.session.room = lobbyPassword;
@@ -217,83 +218,6 @@ app.get('/lobby', (request, response) => {
     else {
         response.redirect('/');
     }
-});
-
-// --- SocketIO ---
-
-io.on('connection', (socket) => {
-    console.log("Socket " + socket.id + " connected.");
-    
-    socket.emit('pushID', socket.id);
-        
-    socket.on('joinLobby', (password) => {
-        socket.join(password); 
-        console.log("Socket joined the room " + password); 
-        
-        users = [];
-        for (let i = 0; i < rooms.length; i++) {
-            if (rooms[i].password === password) {
-                users = rooms[i].users;
-                break;
-            }
-        }
-        socket.emit('joinLobbySuccessful', users);
-    });
-    
-    /*
-    Look for the room in which a player has changed it nickname.
-    Then remove the old nickname and add the new one
-    Also removes and adds the socket id so the nicknames and ids are always in the same place in their arrays.
-    */
-    socket.on('changeNickname', (data) => {
-        let users;
-        let userIDs;
-        for (let i = 0; i < rooms.length; i++) {
-            if (rooms[i].password === data.password) {
-                users = rooms[i].users;
-                userIDs = rooms[i].userIDs;
-                
-                let index = users.indexOf(data.oldNickName);
-                if (index > -1) {
-                    users.splice(index, 1);
-                    userIDs.splice(index, 1);
-                }
-                
-                users.push(data.nickname);
-                userIDs.push(socket.id);
-                rooms[i].users = users;
-                rooms[i].userIDs = userIDs;
-                break;
-            }
-        }
-        io.to(data.password).emit('refreshNicknames', users);
-    });
-    
-    socket.on('disconnect', () => {
-        
-        /*
-        Look if the player that disconnected was in a room and if so remove the player from that room
-        */
-        for (let i = 0; i < rooms.length; i++) {
-            for (let j = 0; j < rooms[i].userIDs.length; j++) {
-                if (rooms[i].userIDs[j] === socket.id) {
-                    
-                    let users = rooms[i].users;
-                    let userIDs = rooms[i].userIDs;
-                    
-                    users.splice(j, 1);
-                    userIDs.splice(j, 1);
-                    
-                    rooms[i].users = users;
-                    rooms[i].userIDs = userIDs;
-                    
-                    io.to(rooms[j].password).emit('refreshNicknames', users);
-                    break;          
-                }
-            }
-        }
-        console.log("Socket " + socket.id + " disconnected.");
-    });
 });
 
 
